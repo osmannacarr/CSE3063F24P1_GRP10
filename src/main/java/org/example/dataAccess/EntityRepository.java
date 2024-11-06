8dcc8edc5daded151b4442f396d09232aa9f57e5
damlakundak <159939168+damlakundak@users.noreply.github.com>
2024-11-06 14:56:44 +0300

    Update EntityRepository.java

--- src/main/java/org/example/dataAccess/EntityRepository.java
+++ src/main/java/org/example/dataAccess/EntityRepository.java
@@ -4,11 +4,15 @@ import org.example.entities.Entity;
 
 import java.util.List;
 
-public interface EntityRepository<T extends Entity>  {
-    //Crud operations
+public interface EntityRepository<T extends BaseEntity> {
+    // Crud operations
     void insert(T entity);
+
     void delete(T entity);
+
     void update(T entity);
+
     T getById(int id);
+
     List<T> getAll();
 }
