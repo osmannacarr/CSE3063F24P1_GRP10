b78cf5db96d98daa3a5214227e4c0c63cfa2e58b
damlakundak <159939168+damlakundak@users.noreply.github.com>
2024-11-06 15:15:38 +0300

    Update StudentDao.java

--- src/main/java/org/example/dataAccess/StudentDao.java
+++ src/main/java/org/example/dataAccess/StudentDao.java
@@ -1,7 +1,8 @@
 package org.example.dataAccess;
 
+import org.example.entities.Course;
 import org.example.entities.Student;
 
 public interface StudentDao extends EntityRepository<Student> {
-
+    void enrollCourse(Student student, Course course);
 }
