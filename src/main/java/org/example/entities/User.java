00255c7128f041e4198746d299ef760af1dc3c90
Osman ACAR <89937161+osmannacarr@users.noreply.github.com>
2024-11-06 18:17:03 +0300

    Update User.java

--- src/main/java/org/example/entities/User.java
+++ src/main/java/org/example/entities/User.java
@@ -1,5 +1,40 @@
 
 public abstract class User extends BaseEntity {
-    string firstName;
-    string lastName;
+String firstName;
+String lastName;
+String password;
+String faculty;
+String department;
+
+public User(String firstName,String lastName,String ID,String password,String faculty,String department) {
+	super(ID);
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.password = password;
+	this.faculty = faculty;
+	this.department = department;
+	
+}
+
+public String getfirstName() {
+	return firstName;
+}
+
+public String getSurname() {
+	return lastName;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public String getFaculty() {
+	return faculty;
+}
+
+public String getDepartment() {
+	return department;
+}
+
+
 }
