aee7ef1ea8f3c9a346849e1dc9e9529f908865eb
damlakundak <159939168+damlakundak@users.noreply.github.com>
2024-11-06 15:15:59 +0300

    Update StudentDaoImpl.java

--- src/main/java/org/example/dataAccess/StudentDaoImpl.java
+++ src/main/java/org/example/dataAccess/StudentDaoImpl.java
@@ -1,8 +1,13 @@
 package org.example.dataAccess;
 
 import org.example.entities.Advisor;
+import org.example.entities.Course;
 import org.example.entities.Student;
 
-public class StudentDaoImpl extends EntityRepositoryBase<Student> implements StudentDao{
+public class StudentDaoImpl extends EntityRepositoryBase<Student> implements StudentDao {
+    public void enrollCourse(Student student, Course course) {
+        student.enrolledCourse.Add(course);
+        
 
+    }
 }
